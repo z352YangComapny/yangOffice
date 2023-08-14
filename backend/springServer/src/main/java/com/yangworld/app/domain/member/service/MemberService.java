29c37f403e553d8f725a9cfa702bafcaba075734
soyoung0420 <my5126@naver.com>
2023-08-14 14:00:41 +0900

    soyoung conflict 해결

--- backend/springServer/src/main/java/com/yangworld/app/domain/member/service/MemberService.java
+++ backend/springServer/src/main/java/com/yangworld/app/domain/member/service/MemberService.java
@@ -3,7 +3,6 @@ package com.yangworld.app.domain.member.service;
 import com.yangworld.app.config.auth.PrincipalDetails;
 import com.yangworld.app.domain.member.dto.SignUpDto;
 import com.yangworld.app.domain.member.dto.UpdateDto;
-import com.yangworld.app.domain.member.entity.Member;
 
 public interface MemberService {
     int insertMember(SignUpDto signUpDto);
