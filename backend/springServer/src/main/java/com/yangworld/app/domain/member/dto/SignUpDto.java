30e0d805c79efa0859aff9f24b87d27947fb30fa
soyoung0420 <my5126@naver.com>
2023-08-18 17:14:50 +0900

    MemberCreate  수정!

--- backend/springServer/src/main/java/com/yangworld/app/domain/member/dto/SignUpDto.java
+++ backend/springServer/src/main/java/com/yangworld/app/domain/member/dto/SignUpDto.java
@@ -1,19 +1,23 @@
 package com.yangworld.app.domain.member.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * 정규식으로 Valid 구체화예정
  * 개발환경에서는 not black 만 으로 설정.
  * */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpDto {
     private int id;
     @NotBlank(message="아이디")
@@ -31,7 +35,7 @@ public class SignUpDto {
     private String phone;
     @NotBlank(message="Email")
     private String email;
-    @NotBlank(message ="생일")
-    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private String birthday;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
 }
