9f3bb0282de44e8bc2b6f44059e9e5e29b46d2d7
ejum01 <ejum0713@gmail.com>
2023-08-20 19:11:17 +0900

    11

--- backend/springServer/src/main/java/com/yangworld/app/config/WebSecurityConfig.java
+++ backend/springServer/src/main/java/com/yangworld/app/config/WebSecurityConfig.java
@@ -43,13 +43,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/member/memberLogin.do")
                 .loginProcessingUrl("/member/memberLogin.do")
                 .usernameParameter("username")
-                .passwordParameter("password")
+                .passwordParameter("password");
 
 //                .defaultSuccessUrl("/member/memberHome.do")
 
-         a7ab4cff61f0fd4138e497aa2e0c9eb3a52f3036
-                .defaultSuccessUrl("/")
-                .permitAll();
+//         a7ab4cff61f0fd4138e497aa2e0c9eb3a52f3036
+//                .defaultSuccessUrl("/")
+//                .permitAll();
 
         http.logout()
                 .logoutUrl("/member/memberLogout.do")
