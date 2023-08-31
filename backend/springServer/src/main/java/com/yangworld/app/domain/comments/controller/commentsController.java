323ace47dbdccc52da7f9cbb99045c367c5061ca
ejum01 <ejum0713@gmail.com>
2023-08-31 23:08:48 +0900

    ㅋㅋ
    ㅋ

--- backend/springServer/src/main/java/com/yangworld/app/domain/comments/controller/commentsController.java
+++ backend/springServer/src/main/java/com/yangworld/app/domain/comments/controller/commentsController.java
@@ -1,4 +1,14 @@
 package com.yangworld.app.domain.comments.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
 public class commentsController {
+
+    //@Autowired
+
 }
