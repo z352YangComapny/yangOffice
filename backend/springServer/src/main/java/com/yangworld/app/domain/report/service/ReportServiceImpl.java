ea114775d4b7eecdd93ab30964b8dfacf520a633
ejum01 <ejum0713@gmail.com>
2023-09-01 14:07:02 +0900

    report

--- backend/springServer/src/main/java/com/yangworld/app/domain/report/service/ReportServiceImpl.java
+++ backend/springServer/src/main/java/com/yangworld/app/domain/report/service/ReportServiceImpl.java
@@ -1,5 +1,9 @@
 package com.yangworld.app.domain.report.service;
 
+import com.yangworld.app.config.auth.PrincipalDetails;
+import com.yangworld.app.domain.photoFeed.entity.PhotoFeed;
+import com.yangworld.app.domain.report.dto.ReportCreateDto;
+import com.yangworld.app.domain.report.dto.ReportFeedRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,9 +14,12 @@ import com.yangworld.app.domain.report.repository.ReportRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.SQLException;
+import java.sql.SQLTransactionRollbackException;
+
 @Service
 @Slf4j
-public class ReportDmServiceImpl implements ReportService {
+public class ReportServiceImpl implements ReportService {
 
 	@Autowired	
 	private ReportRepository reportRepository;
@@ -36,7 +43,39 @@ public class ReportDmServiceImpl implements ReportService {
 	        
 	        return resultReportDm; // insertReportDm 메서드의 결과 반환
 	    }
-	
+
+	@Override
+	public int insertReportFeed(PrincipalDetails principalDetails, ReportFeedRequest feedRequest) {
+
+			int result = 0;
+
+			int reporterId = principalDetails.getId();
+			int feedId = feedRequest.getFeedId();
+			String content = feedRequest.getContent();
+			PhotoFeed photoFeed = reportRepository.findByFeedId(feedId);
+			int reportedId = photoFeed.getWriterId();
+
+			Report report = Report.builder()
+					.reportedId(reportedId)
+					.reporterId(reporterId)
+					.content(content)
+					.build();
+
+			try {
+				result = reportRepository.insertReport(report);
+				if (result > 0) {
+					result += reportRepository.insertReportFeed(feedId);
+					if(result > 1){
+						return result;
+					}
+				}
+			}catch (Exception e){
+				e.printStackTrace();
+				log.debug("안돼..");
+			}
+			return 0;
+	}
+
 //	@Override
 //	public int insertReportDm(ReportDm reportDm) {
 //		int result = reportRepository.insertReportDm(reportDm);
