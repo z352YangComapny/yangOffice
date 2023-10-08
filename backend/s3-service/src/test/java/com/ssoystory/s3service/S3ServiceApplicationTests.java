1436f885ebc26c9b1f0a22f0bd3e16ff9c85b572
GRyu1 <heliotrop1024@gmail.com>
2023-10-08 20:34:08 +0900

    s3-service

---
+++ backend/s3-service/src/test/java/com/ssoystory/s3service/S3ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssoystory.s3service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
