5eda842758074d1d689fc774b4f7ef37de989b36
GRyu1 <heliotrop1024@gmail.com>
2023-10-27 17:40:35 +0900

    account 정리

--- backend/account.sql
+++ backend/account.sql
@@ -1,6 +1,6 @@
---==============================
--- 관리자계정 - spring계정 생성
---==============================
+--===================================
+-- 관리자계정 - spring계정 생성 v0.0.1
+--===================================
 alter session set "_oracle_script" = true;
 
 create user yang
@@ -10,3 +10,28 @@ default tablespace users;
 alter user yang quota unlimited on users;
 
 grant connect, resource to yang;
+
+--===================================
+-- 관리자계정 - spring계정 생성 v1.0.1
+--===================================
+
+--member-service (MySql)
+create user 'yang'@'%' identified by 'yang';
+GRANT ALL PRIVILEGES ON *.* TO 'yang'@'%';
+create database ssoystory_member;
+use ssoystory_member;
+select * from ssoystory_member;
+
+--feed-service (MySql)
+create user 'yang'@'%' identified by 'yang';
+GRANT ALL PRIVILEGES ON *.* TO 'yang'@'%';
+create database ssoystory_feed;
+use ssoystory_feed;
+select * from ssoystory_feed;
+
+--dm-service
+
+--Game (MongoDB)
+
+--payment-service
+
