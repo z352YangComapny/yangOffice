aea532eafe358b56830c887f3434ca4e68f7fecd
GRyu1 <heliotrop1024@gmail.com>
2023-11-27 17:26:04 +0900

    dm init

---
+++ backend/dm-service/src/main/java/com/ssoystory/dmservice/DmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssoystory.dmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmServiceApplication.class, args);
+	}
+
+}
---
+++ backend/dm-service/src/test/java/com/ssoystory/dmservice/DmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssoystory.dmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
